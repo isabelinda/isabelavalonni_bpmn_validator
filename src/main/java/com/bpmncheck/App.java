3390884b1c1847e5e54262d65971936f89928111
isabelinda <isabelinda_rj@hotmail.com>
2018-05-25 17:52:20 -0300

    Add files via upload

--- src/main/java/com/bpmncheck/App.java
+++ src/main/java/com/bpmncheck/App.java
@@ -2,10 +2,6 @@ package com.bpmncheck;
 
 import com.bpmncheck.view.InputFrame;
 
-/**
- * Hello world!
- *
- */
 public class App {
 	public static void main(String[] args) {
 		new InputFrame().setVisible(true);
